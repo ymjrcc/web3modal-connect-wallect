ea63c5f148ba1cbb92d83ba73d57221eba1375e4
heyiming <ymjrcc@qq.com>
2024-07-15 17:20:02 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+'use client'
 import ConnectButton from "@/components/ConnectButton";
+import { useEffect } from "react";
+import { useAccount } from 'wagmi'
 
 export default function Home() {
+  const account = useAccount();
+  // console.log(account);
+
+  useEffect(() => {
+    console.log(account);
+  })
+  
   return (
     <div className='p-4'>
       <ConnectButton />
+
+      <div className='mt-2'>
+        {
+          account.address ?
+          <p>Address：{account.address}</p> :
+          <p className='text-red-600'>Not connected yet, please click the button above to connect your wallet.</p>
+        }
+        
+      </div>
     </div>
   );
 }
