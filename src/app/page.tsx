a5ee68d24efd7b3686170d075d600163a0e24acd
heyiming <ymjrcc@qq.com>
2024-07-15 18:25:08 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,41 @@
 'use client'
 import ConnectButton from "@/components/ConnectButton";
-import { useEffect } from "react";
-import { useAccount } from 'wagmi'
+import { useState } from "react";
+import { useAccountEffect } from 'wagmi'
 
 export default function Home() {
-  const account = useAccount();
-  // console.log(account);
+  const [accountData, setAccountData] = useState<any>(null);
 
-  useEffect(() => {
-    console.log(account);
+  useAccountEffect({
+    onConnect(data) {
+      console.log('Connected!', data)
+      setAccountData(data);
+    },
+    onDisconnect() {
+      console.log('Disconnected!')
+      setAccountData(null);
+    },
   })
   
   return (
     <div className='p-4'>
       <ConnectButton />
-
       <div className='mt-2'>
         {
-          account.address ?
-          <p>Address：{account.address}</p> :
-          <p className='text-red-600'>Not connected yet, please click the button above to connect your wallet.</p>
+          accountData ?
+          <div>
+            <div>Connected Address：{accountData.address}</div>
+            <div>Authorized Addresses：
+              <ul className='list-disc pl-8'>
+                {accountData.addresses.map((address: string) => (
+                  <li key={address}>{address}</li>
+                ))}
+              </ul>
+            </div>
+            <br />
+          </div>:
+          <div className='text-red-600'>Not connected yet, please click the button above to connect your wallet.</div>
         }
-        
       </div>
     </div>
   );
